2a757e66c2488788fe8483d0ba607e4c01795f1d
Nugget-Meister <tremainecamacho@gmail.com>
2023-10-17 21:36:13 -0400

    Added error messages when trying to get bad route

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express")
 
 const transactionsController = require("./controllers/transactions.controller.js")
 
-
+const badQuotesArray = require("./models/badquotes.js")
 
 // Controller Imports
 
@@ -14,8 +14,20 @@ const cors = require("cors")
 app.use(cors())
 app.use(express.json())
 
+
+
+
+
 // Routes
 app.use("/transactions", transactionsController)
 
+
+// Catch all
+app.get("*", (req,res) => {
+    const randomNum = Math.floor(Math.random() * badQuotesArray.length)
+    const quote = badQuotesArray[randomNum]
+    // console.log(randomNum ,quote)
+    res.status(404).json({status: "BAD", message: [quote]})
+})
 
 module.exports = app
---
+++ models/badquotes.js
@@ -0,0 +1,7 @@
+module.exports = [
+    "You're mean. I hate you. I hate you forever and ever and ever.",
+    "I hope monsters come and eat you.",
+    "Hi there.",
+    
+
+]
